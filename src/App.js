b835b4976cabac2b0cc17ac16d682952b0168453
gtinkham <tinky2stax@gmail.com>
2018-09-21 07:17:41 -0700

    fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -68,57 +68,66 @@ export default class Home extends Component {
       signaturesRaw = JSON.stringify(signatures);
     }
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h2>Offline Signing Tool</h2>
-            <h4><a href="https://github.com/titan-digital-exchange/offline-signing-tool/blob/master/README.md">Instructions</a></h4>
+      <div className="container-fluid">
+          <div className="navbar">
+              <img className="navbar-brand img-fluid" style={{width:'4rem'}} src="/icons/Titan_Logo_BrandMark_DropShadow.png" alt=""/>
           </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <form>
-              <div className="form-group">
-                <label>WIF</label>
-                <input className="form-control" value={wif} onChange={this.handleOnChange} type="string" id="wif" />
+          <div className="card w-auto mx-auto" style={{maxWidth: '45rem', padding: '4rem'}}>
+              <div className="row">
+                  <div className="col">
+                      <h2 className="text-center">Offline Signing Tool</h2>
+                      <h4><a href="https://github.com/titan-digital-exchange/offline-signing-tool/blob/master/README.md">Instructions</a></h4>
+                  </div>
               </div>
-              <div className="form-group">
-                <label>Sig Hash (JSON)</label> {/* TODO better file format? */}
-                <input type="file" className="form-control-file"
-                  style={{ marginBottom: '.5rem' }}
-                  onChange={e => {
-                    const file = e.target.files[0];
-                    const fileReader = new FileReader();
-                    fileReader.addEventListener('load', () => this.setState({ sigHashesRaw: fileReader.result }));
-                    fileReader.readAsText(file);
-                  }}
-                ></input>
-                <pre>{sigHashesRaw}</pre>
-                {/* <textarea className="form-control" spellCheck="false" value={sigHashesRaw} onChange={this.handleOnChange} id="sigHashesRaw" rows="3" placeholder="Sig hashes" required></textarea> */}
-                {/* <input className="form-control" value={sigHash} onChange={this.handleOnChange} type="textarea" id="sigHash" /> */}
+              <div className="row">
+                  <div className="col">
+                      <form>
+                          <div className="form-group">
+                              <label>WIF</label>
+                              <input className="form-control" value={wif} onChange={this.handleOnChange} type="string" id="wif" />
+                          </div>
+                          <div className="form-group">
+                              <label>Sig Hash (JSON)</label> {/* TODO better file format? */}
+                              <input type="file" className="form-control-file"
+                                     style={{ marginBottom: '.5rem' }}
+                                     onChange={e => {
+                                         const file = e.target.files[0];
+                                         const fileReader = new FileReader();
+                                         fileReader.addEventListener('load', () => this.setState({ sigHashesRaw: fileReader.result }));
+                                         fileReader.readAsText(file);
+                                     }}
+                              ></input>
+                              <pre>{sigHashesRaw}</pre>
+                              {/* <textarea className="form-control" spellCheck="false" value={sigHashesRaw} onChange={this.handleOnChange} id="sigHashesRaw" rows="3" placeholder="Sig hashes" required></textarea> */}
+                              {/* <input className="form-control" value={sigHash} onChange={this.handleOnChange} type="textarea" id="sigHash" /> */}
+                          </div>
+                          <div className="row">
+                              <div className="orangeButton mx-auto" onClick={this.handleSign}>
+                                  <div style={{margin: 'auto'}}>
+                                      Sign
+                                  </div>
+                              </div>
+                          </div>
+                      </form>
+                      {errorMessage && <div className="alert alert-danger" style={{ marginTop: '1rem' }}>
+                          <h4 className="alert-heading">Error</h4>
+                          {errorMessage}
+                      </div>}
+                      {signaturesRaw && <div className="alert alert-success" style={{ marginTop: '1rem' }}>
+                          <h4 className="alert-heading">Signatures (JSON)</h4>
+                          {signaturesRaw && <div>
+                              <pre className="mb-0">{signaturesRaw}</pre>
+                              <button
+                                  type="button"
+                                  style={{ marginTop: '1rem', marginRight: '.5rem' }}
+                                  className="btn btn-primary"
+                                  onClick={() => this.downloadObjectAsJson(signatures /* not signaturesRaw */, 'signatures')}
+                              >Download File</button>
+                          </div>}
+                      </div>}
+                  </div>
               </div>
-              <button type="button" className="btn btn-block btn-success" onClick={this.handleSign}>
-                Sign
-              </button>
-            </form>
-            {errorMessage && <div className="alert alert-danger" style={{ marginTop: '1rem' }}>
-              <h4 className="alert-heading">Error</h4>
-              {errorMessage}
-            </div>}
-            {signaturesRaw && <div className="alert alert-success" style={{ marginTop: '1rem' }}>
-              <h4 className="alert-heading">Signatures (JSON)</h4>
-              {signaturesRaw && <div>
-                <pre className="mb-0">{signaturesRaw}</pre>
-                <button
-                  type="button"
-                  style={{ marginTop: '1rem', marginRight: '.5rem' }}
-                  className="btn btn-primary"
-                  onClick={() => this.downloadObjectAsJson(signatures /* not signaturesRaw */, 'signatures')}
-                >Download File</button>
-              </div>}
-            </div>}
           </div>
-        </div>
       </div>
     );
   }
